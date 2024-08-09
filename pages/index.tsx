875950c31caf9e8d2cb50d19a6f0a7a5a4e69a0c
Dante Sanchez <dante.a.sanchez@gmail.com>
2024-08-09 16:05:34 -0400

    wonky app

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className={styles.container}>
       <main className={styles.main}>
         <h1 className={styles.title}>
-          This is the conky app!
+          This is the wonky app!
         </h1>
 
         <div className={styles.grid}>
