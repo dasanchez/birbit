6d3830903ba2e83fb62cdc82628687723a7c98d9
Dante Sanchez <dante.a.sanchez@gmail.com>
2024-08-09 14:17:46 -0400

    conky app

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className={styles.container}>
       <main className={styles.main}>
         <h1 className={styles.title}>
-          This is the polypore app!
+          This is the conky app!
         </h1>
 
         <div className={styles.grid}>
